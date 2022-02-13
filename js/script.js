d3406d809f467c080185c1286eb54780d632bea0
kangchaelim <kcl75597639@gmail.com>
2022-02-14 01:27:10 +0900

    cafe knotted

--- js/script.js
+++ js/script.js
@@ -353,6 +353,14 @@ console.log(cake);
 const stollenImg = document.getElementById('stollenImg');
 const bag = document.getElementById('bag');
 const cakeImg = document.getElementById('cakeImg');
+// const imgBox = document.querySelectorAll('body .img-box');
+// console.log(imgBox);
+const shop = document.querySelectorAll('#shop section');
+console.log(shop);
+const hireH = document.querySelector('#hire h1');
+const hireP = document.querySelector('#hire p');
+const carrers = document.querySelector('#hire #carrers');
+
 
 window.addEventListener('scroll', ()=>{
   if(scrollY > 250) {
@@ -362,22 +370,30 @@ window.addEventListener('scroll', ()=>{
     bag.style.marginLeft = '20vw';
   }
 
-  if(scrollY > 600) {
-    cake.style.marginRight = '20vw';
-    cakeImg.style.marginRight = '20vw';
+  if(scrollY > 700) {
+    cake.style.marginRight = '30vw';
+    cakeImg.style.marginRight = '30vw';
   }
   
   if(scrollY > 1440){
     trans[1].style.opacity = 1;
   }
 
-  if(scrollY > 2150){
-    trans[2].style.opacity = 1;
-    trans[3].style.opacity = 1;
-    trans[4].style.opacity = 1;
+  if(scrollY > 1900){
+    // trans[2].style.opacity = 1;
+    // trans[3].style.opacity = 1;
+    // trans[4].style.opacity = 1;
+    for(let i = 0; i < shop.length; i++){
+      setInterval(()=>{
+        shop[i].classList.replace('opa-0', 'opa-1');
+      }, (500) * (i+1));
+    }
   }
 
   if(scrollY > 3000) {
-    trans[5].style.opacity = 1;
+    trans[2].style.opacity = 1;
+    hireH.style.marginTop = '0vh';
+    hireP.style.marginTop = '22vh';
+    carrers.style.marginTop = '27vh';
   }
 });
